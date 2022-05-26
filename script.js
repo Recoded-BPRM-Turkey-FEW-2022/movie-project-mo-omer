e63e09aad30fa5dc18947670a7c4e8b572793982
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-05-27 00:38:45 +0300

    19-done

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict';
-// const API_KEY = 'api_key=5719839582dcc37299e0c1f45ae45110';
+const API_KEY = 'api_key=5719839582dcc37299e0c1f45ae45110';
 // const POPULARITY = TMDB_BASE_URL + '/discover/movie?sort_by=popularity.desc' + API_KEY;
 // const sth = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc/api_key=5719839582dcc37299e0c1f45ae45110'
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w780";
-const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w1280";
+const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
 
+const SEARCH_URL = TMDB_BASE_URL + "/search/movie?" + API_KEY;
+const form = document.getElementById("form");
+const submit = document.getElementById("submit");
 
-// Don't touch this function please
+
 //give any path and it will return the full URL:
 const constructUrl = (path) => {
-  return `${TMDB_BASE_URL}/${path}?api_key=${atob("NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=")}`; 
+  return `${TMDB_BASE_URL}/${path}?api_key=${atob("NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=")}`;
 };
 
 //-----------------------------------------------------------------------------------------------------------------
@@ -44,9 +47,9 @@ logo.addEventListener("click", logoToHome);
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
 const fetchMovies = async () => {
   const url = constructUrl(`movie/now_playing`);
-   const res = await fetch(url);
-    return res.json();
-  };
+  const res = await fetch(url);
+  return res.json();
+};
 
 // // fetch actors that played in the movie(credits):
 // const fetchMovieActors = async () => {
@@ -67,10 +70,11 @@ const renderMovies = (movies) => {
   const mainMovieDiv = document.createElement("div");
   mainMovieDiv.classList.add("row");
   movies.map((movie) => {
-    const movieDiv = document.createElement("div");
-    movieDiv.classList.add("col-md-4", "col-sm-6");
-    movieDiv.innerHTML =
-      `
+    if (movie.poster_path !== null) {
+      const movieDiv = document.createElement("div");
+      movieDiv.classList.add("col-md-4", "col-sm-6");
+      movieDiv.innerHTML =
+        `
       <div class="card m-3" style="width: 20rem;">
         <img src="${BACKDROP_BASE_URL + movie.poster_path}" alt="${movie.title} poster">
           <div class="card-body">
@@ -84,12 +88,13 @@ const renderMovies = (movies) => {
       </div>
       `;
 
-    console.log(movie);
-    movieDiv.addEventListener("click", () => {
-      runMovieDetails(movie);
-    });
-    CONTAINER.appendChild(mainMovieDiv);
-    mainMovieDiv.appendChild(movieDiv);
+      // console.log(movie);
+      movieDiv.addEventListener("click", () => {
+        runMovieDetails(movie);
+      });
+      CONTAINER.appendChild(mainMovieDiv);
+      mainMovieDiv.appendChild(movieDiv);
+    }
   });
 };
 
@@ -97,6 +102,7 @@ const renderMovies = (movies) => {
 // You may need to add to this function, definitely don't delete it.
 const runMovieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
+  // console.log(movieRes);
   renderMovie(movieRes);
 
   const castRes = await fetchMovieActors(movie.id);
@@ -187,7 +193,8 @@ const renderVideos = (movie) => {
 const renderMovieActors = (actor) => {
 
   actor.slice(0, 5).map(element => {
-    CONTAINER.innerHTML += `
+    if (element.profile_path !== null) {
+      CONTAINER.innerHTML += `
     <div class="pt-5 row ">
         <div class="col-md-4">
              <img style="max-width: 60%; height: auto;" src=${PROFILE_BASE_URL + element.profile_path}>
@@ -195,6 +202,7 @@ const renderMovieActors = (actor) => {
         </div>
     </div>
     `;
+    }
   })
 }
 
@@ -203,7 +211,8 @@ const renderSimilarMovies = (movie) => {
   CONTAINER.innerHTML += `<br><h3>Similar Movies: <br></h3>`;
 
   movie.slice(0, 5).map(element => {
-    CONTAINER.innerHTML += `
+    if (element.poster_path !== null) {
+      CONTAINER.innerHTML += `
     <div class="pt-5 row ">
         <div class="col-md-4">
              <img style="max-width: 60%; height: auto;" src=${PROFILE_BASE_URL + element.poster_path}>
@@ -211,6 +220,7 @@ const renderSimilarMovies = (movie) => {
         </div>
     </div>
     `;
+    }
   })
 }
 
@@ -236,14 +246,16 @@ const renderMovie = (movie) => {
   CONTAINER.innerHTML = `
     <div class=" row pt-5">
         <div class="col-md-4">
-             <img id="movie-backdrop" src=${BACKDROP_BASE_URL + movie.backdrop_path
-    }>
+             <img id="movie-backdrop" src=${BACKDROP_BASE_URL + movie.backdrop_path}>
         </div>
         <div class="col-md-8 ">
             <h2 id="movie-title">${movie.title}</h2>
-            <p id="movie-release-date"><b>Release Date:</b> ${movie.release_date
-    }</p>
-            <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
+            <p id="movie-release-date"><b>Release Date:</b> ${movie.release_date}</p>
+            <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p> 
+            <p id="movie-runtime"><b>Language:</b> ${movie.spoken_languages[0].english_name}</p>
+            <p id="movie-runtime"><b>Vote Average:</b> ${movie.vote_average}/10</p>
+            <p id="movie-runtime"><b>Vote Count:</b> ${movie.vote_count}</p>
+
             <h3>Overview:</h3>
             <p id="movie-overview">${movie.overview}</p>
         </div>
@@ -265,7 +277,7 @@ const fetchMoviesByGenre = async () => {
   return json;
 };
 
-const genres =  [
+const genres = [
   {
     "id": 28,
     "name": "Action"
@@ -344,21 +356,22 @@ const genres =  [
   }
 ]
 
+
 const tagsEl = document.getElementById("tags");
 let selectedGenre = []; // WILL STORE ALL THE CLICKED GENRES. SEE EVENT LISTENER BELOW
 const setGenre = () => {
-    genres.forEach( genre => { // LOOP OVER THE ARRAY
+  genres.forEach(genre => { // LOOP OVER THE ARRAY
     const t = document.createElement("div"); // CREATE DIV FOR EACH ARRAY ELEMENT
     t.classList.add("tag");
     t.id = genre.id; //GET THE id VALUES FROM ARRAY AND GIVE TO DOM ELEMENTS
     t.innerText = genre.name; //GET THE name VALUES FROM ARRAY AND GIVE TO DOM ELEMENTS
     t.addEventListener("click", async () => {
-      if (selectedGenre.length == 0){ // IF IT IS EMPTY...
+      if (selectedGenre.length == 0) { // IF IT IS EMPTY...
         selectedGenre.push(genre.id); // ...PUSH THE GENRE ID
       } else { //IF IT IS NOT EMPTY
         if (selectedGenre.includes(genre.id)) { //IF THE GENRE IS ALREADY SELECTED...
           selectedGenre.forEach((id, idx) => { //THEN LOOK AT THE SELECTED GENRES IN THE ARRAY AND...
-            if (id == genre.id){ //IF YOU FIND THE CLICKED GENRE ALREADY IN THE ARRAY...
+            if (id == genre.id) { //IF YOU FIND THE CLICKED GENRE ALREADY IN THE ARRAY...
               selectedGenre.splice(idx, 1); // REMOVE THE GENRE FROM THE ARRAY
             }
           })
@@ -366,7 +379,7 @@ const setGenre = () => {
           selectedGenre.push(genre.id); // THEN SELECT IT (SAVE IT IN THE ARRAY)
         }
       }
-      console.log(selectedGenre);
+      // console.log(selectedGenre);
       const moviesByGenre = await fetchMoviesByGenre();
       renderByGenre(moviesByGenre);
       // console.log(moviesByGenre());
@@ -376,10 +389,12 @@ const setGenre = () => {
 }
 setGenre();
 
+
+
 const renderByGenre = (moviesByGenre) => {
   const genreListDiv = document.createElement("div");
   genreListDiv.classList.add("row");
-  console.log(moviesByGenre);
+  // console.log(moviesByGenre);
   moviesByGenre.results.map((movieByGenre) => {
     const genreDiv = document.createElement("div");
     genreDiv.classList.add("col-md-4", "col-sm-6");
@@ -394,7 +409,7 @@ const renderByGenre = (moviesByGenre) => {
           </div>
       </div>
       `;
-    
+
     genreDiv.addEventListener("click", () => {
       movieDetails(movieByGenre);
     });
@@ -407,7 +422,7 @@ const renderByGenre = (moviesByGenre) => {
 
 
 
-// ----------------------------------- creating functions that fetch and display actors --------------------------------------------
+// -------------------------------------------------- creating functions that fetch and display actors --------------------------------------------
 // runs main page for actors: 
 const runActors = async () => {
   const actors = await fetchActors();
@@ -537,7 +552,7 @@ const renderActor2 = (movie) => {
 
 };
 
-// ------------------------------------------------ Event Listeners ------------------------------------------------------------
+// ------------------------------------------------------------------ Event Listeners ------------------------------------------------------------
 
 let actorsNavBar = document.getElementById("theActors")
 
@@ -606,21 +621,13 @@ document.getElementById("release_date").addEventListener("click", async () => {
   };
   const movies = await fetcher();
   renderMovies(movies.results);
-  console.log(movies.results);
+  // console.log(movies.results);
 })
 
 document.addEventListener("DOMContentLoaded", autorun);
 
-// 1- decide what data you want to fetch, and create a function that fetches it.
-// 2- decide how you want to display it, and create a function that renders it.
-// 3- use the already built constructUrl function to fetch the data.
-// 4- figure out how to style the pages using bootstrap.
 
-// Extra :
-// 1- add a rating color for each movie
-
-
-// ---------------------About us page ---------------------------
+// ------------------------------------------------------About us page ---------------------------------------------------------------------
 
 const aboutUsBtn = document.getElementById("about-us");
 
@@ -644,3 +651,19 @@ aboutUsBtn.addEventListener("click", () => {
 
 
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = submit.value;
+
+  if (searchTerm) {
+
+    const fetcher = async () => {
+      const res = await fetch(SEARCH_URL + '&query=' + searchTerm);
+      return res.json();
+    }
+
+    const movies = await fetcher();
+    renderMovies(movies.results);
+    console.log("searched", movies);
+  }
+});
